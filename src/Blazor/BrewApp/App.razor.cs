b3d2bfa8ded2e69aac8e92c28775d4025703d4d2
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-01-02 19:48:03 +0100

    Start creating Blazor App

---
+++ src/Blazor/BrewApp/App.razor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+using System.Reflection;
+
+namespace BrewApp;
+
+public class AppBase : ComponentBase, IDisposable
+{
+	[Inject] private LazyAssemblyLoader AssemblyLoader { get; set; } = default!;
+	[Inject] private ILogger<App> Logger { get; set; } = default!;
+
+	protected readonly List<Assembly> LazyLoadedAssemblies = new();
+
+	protected async Task OnNavigateAsync(NavigationContext args)
+	{
+		try
+		{
+			switch (args.Path)
+			{
+				case "orders":
+					{
+						var assemblies = await AssemblyLoader.LoadAssembliesAsync(new List<string>
+					{
+						"BrewApp.Modules.Orders.wasm"
+					});
+						LazyLoadedAssemblies.AddRange(assemblies);
+						break;
+					}
+			}
+		}
+		catch (Exception ex)
+		{
+			Logger.LogError($"Error Loading spares page: {ex}");
+		}
+	}
+
+	#region Dispose
+	protected virtual void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			// dispose managed resources
+		}
+	}
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	~AppBase()
+	{
+		Dispose(false);
+	}
+	#endregion
+}
